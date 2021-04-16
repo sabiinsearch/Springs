afdfc09ec3ea9d732854d4b6ecfdfd9f06e9e9ad
sabiinsearch <sabiinsearch@gmail.com>
2021-04-17 00:20:24 +0530

    update

--- gs-spring-boot/complete/src/main/java/com/example/springboot/HelloController.java
+++ gs-spring-boot/complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "new Greetings from Spring Boot!";
+		return "completed Greetings from Spring Boot!";
 	}
 
 }
