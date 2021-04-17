2b5172e333c7adbd48c7b99d1a3e41f69e466e2f
sabiinsearch <sabiinsearch@gmail.com>
2021-04-18 01:15:53 +0530

    update

--- gs-spring-boot/complete/src/app.groovy
+++
@@ -1,9 +0,0 @@
-@RestController
-class ThisWillActuallyRun {
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello, World! located in groovy file in src"
-    }
-
-}
---
+++ gs-spring-boot/complete/src/main/java/com/example/springboot/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.springboot.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "completed Greetings from Spring Boot!";
+	}
+    
+	@RequestMapping("/check")
+	public String test(){
+		return "checking";
+	}
+}
