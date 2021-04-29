84d9fff2b11fe00af9a138a77b6e64d46c6d3c5d
sabiinsearch <sabiinsearch@gmail.com>
2021-04-29 23:20:52 +0530

    update

--- spring-boot-2-mySQL_example/src/main/java/com/example/springboot2mySQL_example/dao/StudentRepo.java
+++ spring-boot-2-mySQL_example/src/main/java/com/example/springboot2mySQL_example/dao/StudentRepo.java
@@ -2,12 +2,13 @@ package com.example.springboot2mySQL_example.dao;
 
 
 import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.springboot2mySQL_example.model.Student;
 
 @Repository 
-public interface StudentRepo extends CrudRepository<Student, Long>
+public interface StudentRepo extends JpaRepository<Student, Long>
 {
    /*Custom Query method to search*/	
    public Student findByIndgNameAndContactNo(String name, String no);
