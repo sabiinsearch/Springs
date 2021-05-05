8ff6aace1b81ef76298639e4663ff4039cc48618
sabiinsearch <sabiinsearch@gmail.com>
2021-05-05 22:28:06 +0530

    update

---
+++ gs-spring-boot/FileOtherAppPropertySourceConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.springboot2mySQL_example.config;
+
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@PropertySource ("file:/home/sarbjeet/github/Springs/myApplication.properties")
+public class PropertySourceConfiguration {
+ 
+}
